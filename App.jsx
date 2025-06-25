8c90d9142953eb0a9f739b7b8f157d2bc22ed522
Grind22AMB8687 <clifflindsey45@gmail.com>
2025-06-25 00:39:59 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black text-white text-2xl font-bold">
+      FlexGrind Mindset App 🚀
+    </div>
+  );
+}
+
+export default App;
